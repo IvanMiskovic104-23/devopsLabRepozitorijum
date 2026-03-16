70342e1165b86e3f36988f9c0d3674fe5c42f63d
IvanMiskovic104-23 <ivan.miskovic.104.23@ict.edu.rs>
2026-03-16 16:41:33 +0100

    Create devopsLab.c

---
+++ devopsLab.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    char ime[20];
+    char prezime[20];
+    char indeks[10];
+    int godina;
+
+    printf("Unesite ime: ");
+    scanf("%s", ime);
+
+    printf("Unesite prezime: ");
+    scanf("%s", prezime);
+
+    printf("Unesite broj indeksa: ");
+    scanf("%s", indeks);
+
+    printf("Unesite godinu rodjenja: ");
+    scanf("%d", &godina);
+
+    printf("\n Informacije o studentu: \n");
+    printf("Ime i prezime: %s %s\n", ime, prezime);
+    printf("Broj indeksa: %s\n", indeks);
+    printf("Godina rodjenja: %d\n", godina);
+
+    if ((godina % 4 == 0 && godina % 100 != 0) || (godina % 400 == 0)) {
+        printf("Rodjen si u prestupnoj godini.\n");
+    } else {
+        printf("Nisi rodjen u prestupnoj godini.\n");
+    }
+    return 0;
+}
